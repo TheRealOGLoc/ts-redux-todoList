57906f1077ac318a1b7bd91203221c94710b6f17
Yuchen Liu <lyc990312@hotmail.com>
2024-07-04 19:55:56 +1000

    Create the todo slice

---
+++ src/state/todo/todoSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+interface todoState {
+  todos: string[]
+}
+
+const initialState: todoState = {
+  todos: []
+}
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      state.todos.push(action.payload)
+    },
+    removeTodoByIndex: (state, action: PayloadAction<Number>) => {
+      state.todos.filter((_, index) => index !== action.payload)
+    }
+  }
+})
+
+export const { addTodo, removeTodoByIndex } = todoSlice.actions
+export default todoSlice.reducer
